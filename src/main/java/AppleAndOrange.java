37ab92b23dab7c3821419076dc002c27051b33c5
PauFregi <paulfrancis.ellis@gmail.com>
2017-06-12 01:06:35 +0100

    Apple and Orange

---
+++ src/main/java/AppleAndOrange.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+/*
+https://www.hackerrank.com/challenges/apple-and-orange
+
+Sam's house has an apple tree and an orange tree that yield an abundance of fruit.
+In the diagram below, the red region denotes his house, where s is the start point and t is the end point.
+The apple tree is to the left of his house, and the orange tree is to its right.
+You can assume the trees are located on a single point, where the apple tree is at point a and the orange tree is at point b.
+
+When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis.
+A negative value of  means the fruit fell d units to the tree's left,
+and a positive value of d means it falls d units to the tree's right.
+
+Given the value of d for m apples and n oranges,
+can you determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range [s,t])?
+
+Print the number of apples that fall on Sam's house as your first line of output,
+then print the number of oranges that fall on Sam's house as your second line of output.
+
+ * Input Format
+
+The first line contains two space-separated integers denoting the respective values of s and t.
+The second line contains two space-separated integers denoting the respective values of a and b.
+The third line contains two space-separated integers denoting the respective values of m and n.
+The fourth line contains m space-separated integers denoting the respective distances that each apple falls from point a.
+The fifth line contains n space-separated integers denoting the respective distances that each orange falls from point b.
+
+ * Constraints
+
+1 <= s,t,a,b,,m,n <= 10000
+-10000 <=d <= 10000
+a < s < t < b
+
+ * Output Format
+
+Print two lines of output:
+On the first line, print the number of apples that fall on Sam's house.
+On the second line, print the number of oranges that fall on Sam's house.
+ */
+
+public class AppleAndOrange {
+
+    public static int countFruits(int[] fruits, int tree, int houseStart, int houseEnd) {
+        int count = 0;
+
+        for(int fruit: fruits){
+            int fruitPos = tree + fruit;
+            if (fruitPos >= houseStart && fruitPos <= houseEnd) count ++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int t = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int[] apple = new int[m];
+        for (int apple_i = 0; apple_i < m; apple_i++) {
+            apple[apple_i] = in.nextInt();
+        }
+        int[] orange = new int[n];
+        for (int orange_i = 0; orange_i < n; orange_i++) {
+            orange[orange_i] = in.nextInt();
+        }
+
+        System.out.println(countFruits(apple, a, t, s));
+        System.out.println(countFruits(orange, b, t, s));
+    }
+}
---
+++ src/test/java/AppleAndOrangeTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleAndOrangeTest {
+
+    @Test
+    public void noFruits() throws Exception {
+        int[] apples = new int[]{};
+        int appleA = 5;
+        int houseS = 7;
+        int houseT = 11;
+
+        int expected = 0;
+
+        int result = AppleAndOrange.countFruits(apples, appleA, houseS, houseT);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void countFruits() throws Exception {
+        int[] apples = new int[]{-2, 2, 1};
+        int appleA = 5;
+        int houseS = 7;
+        int houseT = 11;
+
+        int expected = 1;
+
+        int result = AppleAndOrange.countFruits(apples, appleA, houseS, houseT);
+
+        assertEquals(expected, result);
+    }
+
+}
